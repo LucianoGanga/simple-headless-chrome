2a9202cd29fb41b5b63f9cb9d0100e97f1ecc345
Marcel <MTRNord@users.noreply.github.com>
2017-07-31 14:25:06 +0200

    Fix Example to match new syntax

--- examples/screenCapture.js
+++ examples/screenCapture.js
@@ -5,14 +5,18 @@ const browser = new HeadlessChrome({
 })
 async function navigateWebsite () {
   await browser.init()
+  
+  //Open a new Tab 
+  const mainTab = await browser.newTab({ privateTab: false })
+  
   // Navigate to a URL
-  await browser.goTo('https://github.com/LucianoGanga/simple-headless-chrome')
+  await mainTab.goTo('https://github.com/LucianoGanga/simple-headless-chrome')
 
   // Resize the viewport to full screen size (One use is to take full size screen shots)
   // await browser.resizeFullScreen()
 
   // Take a screenshot
-  await browser.saveScreenshot('./google')
+  await mainTab.saveScreenshot('./google')
 
   // Close the browser
   await browser.close()
